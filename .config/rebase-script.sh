0d3bff48db2b040facf0432af10fa9b104793d37
udaypatel1 <uday3patel@gmail.com>
2024-02-23 20:24:10 -0500

    fix/hasd: fixinggggg

--- .config/rebase-script.sh
+++ .config/rebase-script.sh
@@ -5,7 +5,7 @@ commits=$(git rev-list --count @{u}..HEAD)
 
 # Check if there are any commits to rebase
 if [ $commits -eq 1 ]; then
-    echo "\nNo local commits to squash\n"
+    echo -e "\nNo local commits to squash\n"
     exit 0
 fi
 
@@ -13,10 +13,10 @@ fi
 git reset --soft HEAD~$((commits-1))
 
 # Prompt user to enter a new commit message for the squashed commit
-echo "\nEnter a new commit message for the squashed commit:\n"
+echo -e "\nEnter a new commit message for the squashed commit:\n"
 read new_commit_message
 
 # Amend the squashed commit with the new commit message
 git commit --amend -m "$new_commit_message"
 
-echo "\nRebase Complete! Squashed commits and amended message\n"
+echo -e "\nRebase Complete! Squashed commits and amended message\n"
